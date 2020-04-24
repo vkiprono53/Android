f009c8b3033320109934f2c1e05b37b6dd69217e
vkiprono53 <kipronovincent53@gmail.com>
2020-04-24 18:18:42 +0300

    added menu

--- app/src/main/java/com/vkiprono/instagramclone/HomeActivity.kt
+++ app/src/main/java/com/vkiprono/instagramclone/HomeActivity.kt
@@ -2,11 +2,17 @@ package com.vkiprono.instagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
